75e25e981f45fa8d429e7df115d8402c5fdcd546
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 15:07:20 -0300

    Solution to the problem 2786 - School Floor

---
+++ src/SchoolFloor.2786/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SchoolFloor._2786
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            int firstTypeTiles = (l * c) + ((l - 1) * (c - 1));
+            int secondTypeTiles = (2 * (l - 1)) + (2 * (c - 1));
+
+            if (firstTypeTiles < 0)
+            {
+                firstTypeTiles = 0;
+            }
+
+            if (secondTypeTiles < 0)
+            {
+                secondTypeTiles = 0;
+            }
+
+            Console.WriteLine(firstTypeTiles);
+            Console.WriteLine(secondTypeTiles);
+        }
+    }
+}
