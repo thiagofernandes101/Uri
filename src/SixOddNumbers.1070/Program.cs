7216ec05b06f4e3df37edb7b85860ca3343ffa5e
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 13:33:42 -0300

    Solution to the problem 1070 - Six Odd Numbers

---
+++ src/SixOddNumbers.1070/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SixOddNumbers._1070
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                x += 1;
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine(x);
+                x += 2;
+            }
+        }
+    }
+}
