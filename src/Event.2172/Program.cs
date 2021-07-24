643f71e2f44c52891192c7564adf4e3f386fd62e
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 20:28:47 -0300

    Solution to the problem 2172 - Event

---
+++ src/Event.2172/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Event._2172
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testCase;
+            List<double> exps = new List<double>();
+
+            for (; ; )
+            {
+                testCase = Console.ReadLine();
+
+                if (testCase != "0 0")
+                {
+                    double x = Convert.ToDouble(testCase.Split(' ')[0]);
+                    double m = Convert.ToDouble(testCase.Split(' ')[1]);
+                    double expIncrease = x * m;
+
+                    exps.Add(expIncrease);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            exps.ForEach(value => Console.WriteLine(value));
+        }
+    }
+}
