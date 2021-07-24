f1aa4f66537e71ee8510bcfb14839f8e6c2b1d5b
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 20:38:50 -0300

    Solution to the problem 2747 - Output 1

---
+++ src/Output1.2747/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Output1._2747
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("|                                     |");
+            Console.WriteLine("---------------------------------------");
+        }
+    }
+}
