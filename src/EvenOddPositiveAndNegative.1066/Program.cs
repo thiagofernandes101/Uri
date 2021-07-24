c1250911f7e5f3827667f8f0c09e7237a7f8e9b4
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 16:15:22 -0300

    Solution to the problem 1066 - Even, Odd, Positive and Negative

---
+++ src/EvenOddPositiveAndNegative.1066/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace EvenOddPositiveAndNegative._1066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfEvenValues = 0;
+            int numberOfOddValues = 0;
+            int numberOfPositiveValues = 0;
+            int numberOfNegativeValues = 0;
+
+            int[] values = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                values[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                int value = values[i];
+
+                if (value % 2 == 0)
+                {
+                    numberOfOddValues++;
+                }
+                else
+                {
+                    numberOfEvenValues++;
+                }
+
+                if (value > 0)
+                {
+                    numberOfPositiveValues++;
+                }
+                else if (value < 0)
+                {
+                    numberOfNegativeValues++;
+                }
+            }
+
+            Console.WriteLine($"{numberOfOddValues} valor(es) par(es)");
+            Console.WriteLine($"{numberOfEvenValues} valor(es) impar(es)");
+            Console.WriteLine($"{numberOfPositiveValues} valor(es) positivo(s)");
+            Console.WriteLine($"{numberOfNegativeValues} valor(es) negativo(s)");
+        }
+    }
+}
