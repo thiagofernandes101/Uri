3b69d1382cfc7bfc6f9454a463269232d48ab31f
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 16:43:53 -0300

    Solution to the problem 1759 - Ho Ho Ho

---
+++ src/HoHoHo.1759/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HoHoHo._1759
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    Console.WriteLine("Ho!");
+                }
+                else
+                {
+                    Console.Write("Ho ");
+                }
+            }
+        }
+    }
+}
