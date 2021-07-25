1a630d4d4143b303981d5c83f56b684ac58e1393
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-24 21:16:44 -0300

    Solution to the problem 1019 - Time Conversion

---
+++ src/TimeConversion.1019/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimeConversion._1019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            
+            int hour = Convert.ToInt32(Math.Floor((double)seconds / 3600));
+            seconds %= 3600;
+
+            int minutes = Convert.ToInt32(Math.Floor((double)seconds / 60));
+            seconds %= 60;
+
+            Console.WriteLine($"{hour}:{minutes}:{seconds}");
+        }
+    }
+}
