f73ecfa20043c990afa0580a8a4dbd3a7dfad51b
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 16:12:26 -0300

    Solution to the problem 1073 - Even Square

---
+++ src/EvenSquare.1073/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EvenSquare._1073
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 2; i <= n; i += 2)
+            {
+                Console.WriteLine($"{i}^2 = {Math.Pow(i, 2.0)}");
+            }
+        }
+    }
+}
