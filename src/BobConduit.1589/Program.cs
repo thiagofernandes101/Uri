9d0f9b007ae56a974261a0ba29ee701480cd99d2
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 16:33:22 -0300

    Solution to the problem 1589 - Bob Conduit

---
+++ src/BobConduit.1589/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BobConduit._1589
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            int[] circularConduitsRadius = new int[t];
+
+            for (int i = 0; i < t; i++)
+            {
+                string[] testCase = Console.ReadLine().Split(' ');
+                circularConduitsRadius[i] = Convert.ToInt32(testCase[0]) + Convert.ToInt32(testCase[1]);
+            }
+
+            foreach (int circularConduitRadius in circularConduitsRadius)
+            {
+                Console.WriteLine(circularConduitRadius);
+            }
+        }
+    }
+}
