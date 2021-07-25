03eeaeabb5336b5b441b3e952b2b290d0b84ee36
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 15:41:52 -0300

    Solution to the problem 1078 - Multiplication Table

---
+++ src/MultiplicationTable.1078/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultiplicationTable._1078
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i} x {value} = {i * value}");
+            }
+        }
+    }
+}
