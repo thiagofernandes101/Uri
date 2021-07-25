c72e73002bf7e4c488668bb4bd5686136b224771
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 17:52:03 -0300

    Solution to the problem 1115 - Quadrant

---
+++ src/Quadrant.1115/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Quadrant._1115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                string[] testCase = Console.ReadLine().Split(' ');
+                
+                int x = Convert.ToInt32(testCase[0]);
+                int y = Convert.ToInt32(testCase[1]);
+
+                if (x > 0 && y > 0)
+                {
+                    Console.WriteLine("primeiro");
+                }
+                else if (x < 0 && y > 0)
+                {
+                    Console.WriteLine("segundo");
+                }
+                else if (x < 0 && y < 0)
+                {
+                    Console.WriteLine("terceiro");
+                }
+                else if (x > 0 && y < 0)
+                {
+                    Console.WriteLine("quarto");
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
