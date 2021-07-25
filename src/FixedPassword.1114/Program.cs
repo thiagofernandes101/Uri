3356841ae21610329b3cf22cd24dd37aa9ca71bf
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 16:48:42 -0300

    Solution to the problem 1114 - Fixed Password

---
+++ src/FixedPassword.1114/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FixedPassword._1114
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                if (Convert.ToInt32(Console.ReadLine()) == 2002)
+                {
+                    Console.WriteLine("Acesso Permitido");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Senha Invalida");
+                }
+            }
+        }
+    }
+}
