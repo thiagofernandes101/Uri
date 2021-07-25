b7a7600cedaba077547d2468de61c13935b8766b
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-07-25 15:50:03 -0300

    Solution to the problem 3302 - Correct Answer

---
+++ src/CorrectAnswer.3302/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CorrectAnswer._3302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> answers = new List<string>();
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int entry = Convert.ToInt32(Console.ReadLine());
+
+                answers.Add($"resposta {i}: {entry}");
+            }
+
+            answers.ForEach(answer => Console.WriteLine(answer));
+        }
+    }
+}
