a40c3ed206e38f37bd1b74a81c9e824ca82926ac
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-10-02 18:39:25 -0300

    Solution to the problem 1930 Electrical Outlet

---
+++ src/ElectricalOutlet.1930/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ElectricalOutlet._1930
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberOfTakenFromEachFourStrips = Console.ReadLine().Split(' ');
+            int maximumNumberOfDevicesThatCanBeConnected = 0;
+
+            for(int i = 0; i < numberOfTakenFromEachFourStrips.Length; i++)
+            {
+                if (i == numberOfTakenFromEachFourStrips.Length - 1)
+                {
+                    maximumNumberOfDevicesThatCanBeConnected += Convert.ToInt32(numberOfTakenFromEachFourStrips[i]);
+                }
+                else
+                {
+                    maximumNumberOfDevicesThatCanBeConnected += Convert.ToInt32(numberOfTakenFromEachFourStrips[i]) - 1;
+                }
+            }
+
+            Console.WriteLine(maximumNumberOfDevicesThatCanBeConnected);
+        }
+    }
+}
