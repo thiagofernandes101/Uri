1adb8e0b185e34ae5233137355c7cb7382e792f9
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-11-26 19:25:07 -0300

    Solution to the problem 2748 - Output 2

---
+++ src/Output2.2748/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Output2._2748
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string output = string.Empty;
+            output += "---------------------------------------" + Environment.NewLine;
+            output += "|        Roberto                      |" + Environment.NewLine;
+            output += "|                                     |" + Environment.NewLine;
+            output += "|        5786                         |" + Environment.NewLine;
+            output += "|                                     |" + Environment.NewLine;
+            output += "|        UNIFEI                       |" + Environment.NewLine;
+            output += "---------------------------------------" + Environment.NewLine;
+
+            Console.Write(output);
+        }
+    }
+}
