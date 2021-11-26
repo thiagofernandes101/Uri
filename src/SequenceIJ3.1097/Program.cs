0be4689d175914752ad32c96fc04ed02b2697ef8
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-11-26 19:45:00 -0300

    Solution to the problem 1097 - Sequence IJ 3

---
+++ src/SequenceIJ3.1097/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SequenceIJ3._1097
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 7;
+
+            for (int i = 1; i <= 9; i+=2)
+            {
+                for (int counter = 1; counter <= 3; counter++)
+                {
+                    Console.WriteLine($"I={i} J={j}");
+                    j--;
+                }
+
+                j += 5;
+            }
+        }
+    }
+}
