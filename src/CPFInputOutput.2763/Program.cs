a365485bce82014a1c80a6c4335b1e912f0051c0
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-11-27 15:06:18 -0300

    Solution to the problem 2763 - CPF Input Output

---
+++ src/CPFInputOutput.2763/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CPFInputOutput._2763
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cpf = Console.ReadLine();
+            string[] cpfParts = cpf.Split(new char[] { '.', '-' });
+
+            foreach (string part in cpfParts)
+            {
+                Console.WriteLine(part);
+            }
+        }
+    }
+}
