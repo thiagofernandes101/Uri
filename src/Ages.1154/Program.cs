ccb6771c2c179e935696262716076cadd26209de
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-11-28 01:01:13 -0300

    Solution to the problem 1154 - Ages

---
+++ src/Ages.1154/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ages._1154
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ages = new List<int>();
+            int age = 0;
+
+            while (age >= 0)
+            {
+                age = Convert.ToInt32(Console.ReadLine());
+
+                if (age >= 0)
+                {
+                    ages.Add(age);
+                }
+            }
+
+            double ageAverage = ages.Sum() / (ages.Count() * 1.0);
+            Console.WriteLine("{0:f2}", ageAverage);
+            Console.ReadLine();
+        }
+    }
+}
