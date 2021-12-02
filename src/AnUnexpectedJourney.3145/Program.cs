4675a8b65087280bc02a00e2425939d8458c6c48
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-12-01 21:13:39 -0300

    Solution to the problem 3145 - An unexpected Journey

---
+++ src/AnUnexpectedJourney.3145/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AnUnexpectedJourney._3145
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string numberOfDwarvesAndDistance = Console.ReadLine();
+            int numberOfDwarves = Convert.ToInt32(numberOfDwarvesAndDistance.Split(' ')[0]);
+            int distance = Convert.ToInt32(numberOfDwarvesAndDistance.Split(' ')[1]);
+            double timeToReachTheGoal = distance / (numberOfDwarves + 2.0);
+            Console.WriteLine("{0:f2}", timeToReachTheGoal);
+            Console.ReadLine();
+        }
+    }
+}
