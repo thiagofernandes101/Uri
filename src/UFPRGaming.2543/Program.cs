44feed170d60bc4465ce9c1c3ec183d0de485e77
thiagofernandes101 <thiagofernandes101@outlook.com>
2021-12-05 19:47:14 -0300

    Solution to the problem 2543 - UFPR Gaming

---
+++ src/UFPRGaming.2543/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace UFPRGaming._2543
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testCase = string.Empty;
+
+            while (testCase != "EOF")
+            {
+                testCase = Console.ReadLine();
+
+                if (testCase == "EOF")
+                {
+                    break;
+                }
+
+                int numberOfGameplaysPublished = Convert.ToInt32(testCase.Split(' ')[0]);
+                long studentUniversityId = Convert.ToInt32(testCase.Split(' ')[1]);
+                int numberOfContraStrikeVideosPublished = 0;
+
+                for (int i = 0; i < numberOfGameplaysPublished; i++)
+                {
+                    string videoDescription = Console.ReadLine();
+                    bool isAuthorUniversityId = Convert.ToInt32(videoDescription.Split(' ')[0]) == studentUniversityId;
+                    bool isContraStrikeGameplay = Convert.ToInt32(videoDescription.Split(' ')[1]) == 0;
+
+                    if (isAuthorUniversityId && isContraStrikeGameplay)
+                    {
+                        numberOfContraStrikeVideosPublished++;
+                    }
+                }
+
+                Console.WriteLine(numberOfContraStrikeVideosPublished);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
